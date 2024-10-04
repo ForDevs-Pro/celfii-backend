1c513d33b77ff34a9262cfd7c11197c7c9c69211
Mika <test@Tests-MacBook-Pro.local>
2024-10-04 12:59:00 -0300

    feat(models): add Product and Category models

---
+++ src/models/Category.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Category",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      isActive: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+    },
+    { timestamps: true }
+  );
+};
