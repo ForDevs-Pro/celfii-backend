b0f8e9bd2f48df6dc930a3621a932d0baa71f4b2
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-10-27 21:49:54 -0300

    fix(product-ordering): apply global sorting for most popular products

--- src/utils/product-util.js
+++ src/utils/product-util.js
@@ -1,21 +1,21 @@
-const { createView } = require("../controllers/view-controller");
-const { createCategoryController } = require("../controllers/category-controller");
-const { uploadImages, deleteImages } = require("../controllers/image-controller");
+const { createView } = require('../controllers/view-controller');
+const { createCategoryController } = require('../controllers/category-controller');
+const { uploadImages, deleteImages } = require('../controllers/image-controller');
 
-const { Product, View, Image, Category } = require("../db");
-const { Op } = require("sequelize");
+const { Product, View, Image, Category, sequelize } = require('../db');
+const { Op } = require('sequelize');
 
 const orderOptions = {
-  "most popular": [["view", "counter", "DESC"]],
-  "highest price": [["priceArs", "DESC"]],
-  "lowest price": [["priceArs", "ASC"]],
-  newest: [["createdAt", "DESC"]],
+  'most popular': [[{ model: View, as: 'view' }, 'counter', 'DESC']],
+  'highest price': [['priceArs', 'DESC']],
+  'lowest price': [['priceArs', 'ASC']],
+  newest: [['createdAt', 'DESC']],
 };
 
 const getProductIncludes = () => [
-  { model: View, as: "view" },
-  { model: Image, as: "images" },
-  { model: Category, as: "category" },
+  { model: View, as: 'view' },
+  { model: Image, as: 'images' },
+  { model: Category, as: 'category' },
 ];
 
 const getProductData = ({
@@ -29,27 +29,35 @@ const getProductData = ({
   category,
 }) => {
   const paranoid = !onlyDeleted;
-  const order = orderOptions[sort] || [];
-  const include = [
-    { model: View, as: "view" },
-    { model: Image, as: "images" },
-    { model: Category, as: "category", required: !!category },
-  ];
   const limit = Math.max(parseInt(perPage, 10), 1);
   const offset = Math.max((parseInt(page, 10) - 1) * limit, 0);
+
   const where = {
     ...(onlyDeleted ? { deletedAt: { [Op.ne]: null } } : {}),
     ...(name && { [Op.or]: [{ name: { [Op.iLike]: `%${name}%` } }] }),
     ...(minPrice && { priceArs: { [Op.gte]: parseFloat(minPrice) } }),
     ...(maxPrice && { priceArs: { [Op.lte]: parseFloat(maxPrice) } }),
-    ...(category && { "$category.name$": { [Op.iLike]: `%${category}%` } }),
+    ...(category && { '$category.name$': { [Op.iLike]: `%${category}%` } }),
   };
+
+  const include = [
+    { model: View, as: 'view' },
+    { model: Image, as: 'images' },
+    { model: Category, as: 'category', required: !!category },
+  ];
+
+  const order =
+    sort === 'most popular'
+      ? sequelize.literal(
+          `(SELECT "counter" FROM "Views" WHERE "Views"."productId" = "Product"."id") DESC`
+        )
+      : orderOptions[sort] || [];
 
   return { limit, offset, order, where, include, paranoid };
 };
 
 const formatImeiWithSpaces = (imei) => {
-  const cleanedImei = imei.replace(/\s/g, "");
+  const cleanedImei = imei.replace(/\s/g, '');
   if (cleanedImei.length === 15)
     return `${cleanedImei.slice(0, 2)} ${cleanedImei.slice(2, 8)} ${cleanedImei.slice(
       8,
@@ -73,7 +81,7 @@ const addProductAssociations = async ({ id, category, images }) => {
       await product.setCategory(categoryInstances);
     }
   } catch (error) {
-    console.error("Error adding associations:", error.message);
+    console.error('Error adding associations:', error.message);
     throw new Error(`Error adding associations: ${error.message}`);
   }
 };
@@ -81,10 +89,10 @@ const addProductAssociations = async ({ id, category, images }) => {
 const setProductAssociations = async ({ id, category, images, imagesToDelete }) => {
   try {
     if (imagesToDelete) await deleteImages(imagesToDelete);
-    
-    const product = await Product.findByPk(id , { paranoid: false });
 
-    if (images && typeof images === "object") {
+    const product = await Product.findByPk(id, { paranoid: false });
+
+    if (images && typeof images === 'object') {
       const imagesInstances = await uploadImages(id, images);
       await product.addImages(imagesInstances);
     }
@@ -94,7 +102,7 @@ const setProductAssociations = async ({ id, category, images, imagesToDelete }) 
       await product.setCategory(categoryInstances);
     }
   } catch (error) {
-    console.error("Error setting associations:", error.message);
+    console.error('Error setting associations:', error.message);
     throw new Error(`Error setting associations: ${error.message}`);
   }
 };
